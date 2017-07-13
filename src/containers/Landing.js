b94ba8e9f6a642958674ef9a9481c38c7537c8ea
Alexey Smolyakov <smolyakov.alexey@gmail.com>
2017-07-13 14:46:11 +0300

    Landing.

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -3,19 +3,36 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { login } from '../actions/AuthActions'
 
+import preview00 from '../assets/preview-00.png'
+import preview01 from '../assets/preview-01.png'
+import preview02 from '../assets/preview-02.png'
+
+import likeometer from '../assets/likeometer.svg'
+
 class Landing extends Component {
+	constructor () {
+		super();
+		const min = 0;
+		const max = 2;
+		const previewIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+		this.preview = [preview00, preview01, preview02][previewIndex];
+	}
+
+
 	onLoginClick () {
 		this.props.login();
 	}
 
 	render () {
+		const style = { backgroundImage: `url(${this.preview})` };
 		return (
-			<div>
-				<div>
-					Landing{this.props.checkingAuth && `, checking auth`}
+			<div className="landing">
+				<div className="preview" style={style}/>
+				<div className="likeometer">
+					<img src={likeometer} alt=""/>
 				</div>
-				<div>
-					<button type="button" onClick={::this.onLoginClick}>Login VK</button>
+				<div className="login-button">
+					<button type="button" onClick={::this.onLoginClick}>Войти через <i className="fa fa-vk"/></button>
 				</div>
 			</div>
 		);
