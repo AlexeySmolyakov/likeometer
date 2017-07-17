2fff91b1524285fa1ea6568446403ec257ff96a3
Alexey Smolyakov <smolyakov.alexey@gmail.com>
2017-07-17 13:23:12 +0300

    Landing, svg images.

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -3,19 +3,32 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { login } from '../actions/AuthActions'
 
-import preview00 from '../assets/preview-00.png'
-import preview01 from '../assets/preview-01.png'
-import preview02 from '../assets/preview-02.png'
+import splash02 from '../assets/splash-02.svg'
+import splash03 from '../assets/splash-03.svg'
+import splash04 from '../assets/splash-04.svg'
+import splash05 from '../assets/splash-05.svg'
+import splash06 from '../assets/splash-06.svg'
+import splash07 from '../assets/splash-07.svg'
 
 import likeometer from '../assets/likeometer.svg'
 
 class Landing extends Component {
 	constructor () {
 		super();
+
+		const images = [
+			splash02,
+			splash03,
+			splash04,
+			splash05,
+			splash06,
+			splash07,
+		];
 		const min = 0;
-		const max = 2;
+		const max = images.length - 1;
+
 		const previewIndex = Math.floor(Math.random() * (max - min + 1)) + min;
-		this.preview = [preview00, preview01, preview02][previewIndex];
+		this.preview = images[previewIndex];
 	}
 
 
@@ -24,12 +37,13 @@ class Landing extends Component {
 	}
 
 	render () {
-		const style = { backgroundImage: `url(${this.preview})` };
 		return (
 			<div className="landing">
-				<div className="preview" style={style}/>
 				<div className="likeometer">
 					<img src={likeometer} alt=""/>
+				</div>
+				<div className="preview">
+					<img src={this.preview} alt="Likeometer"/>
 				</div>
 				<div className="login-button">
 					<button type="button" onClick={::this.onLoginClick}>Войти через <i className="fa fa-vk"/></button>
