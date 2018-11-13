d862451d56ead4bdb9f7bd32b2d3ec2272c87604
Alexey Smolyakov <smolyakov.alexey@gmail.com>
2018-11-13 09:18:26 +0300

    updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, Link } from 'react-router-dom';
+import block from 'bem-cn-lite';
 
-import likeometer from '../../assets/likeometer.svg';
+import likeometer from '../../assets/likeometer2-02.svg';
 import './styles.scss';
 
 class Header extends Component {
   render() {
     const { user } = this.props;
+
+    const b = block('Header');
     const image = { backgroundImage: `url(${user.photo_200_orig})` };
 
     return (
-      <header>
+      <div className={b()}>
         <div className="wrap">
-          <Link to={`/albums${user.id}`} className="likeometer">
+          <Link to={`/albums${user.id}`} className={'likeometer'}>
             <img src={likeometer} alt="Likeometer" />
           </Link>
-          <NavLink className="links" to={`/albums${user.id}`}>фото</NavLink>
-          <NavLink className="links" to='/friends'>друзья</NavLink>
-          <NavLink className="links" to='/groups'>группы</NavLink>
-          {user.id ?
-            <div className="user">
-              <div>{user.first_name}</div>
-              <div className="image" style={image} />
-            </div> :
-            <span className="links">Вход</span>
-          }
+          <NavLink className="links" to={`/albums${user.id}`}>
+            <i className="fas fa-camera" />
+            <span>фото</span>
+          </NavLink>
+          <NavLink className="links" to='/friends'>
+            <i className="fas fa-user" />
+            <span>друзья</span>
+          </NavLink>
+          <NavLink className="links" to='/groups'>
+            <i className="fas fa-user-friends" />
+            <span>группы</span>
+          </NavLink>
+          <div className="user">
+            <div>{user.first_name}</div>
+            <div className="image" style={image} />
+          </div>
         </div>
-      </header>
+      </div>
     );
   }
 }
