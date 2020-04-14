e5a4b77d9c6450443dfc3d9c723294f2223fb3f5
Aleksei Smoliakov <aleksei.smoliakov@domrf.ru>
2020-04-15 00:13:53 +0300

    fix: router

--- src/Root.js
+++ src/Root.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 
 import AlbumsNext from 'containers/AlbumsNext';
 import PhotosNext from 'containers/PhotosNext';
 
-const Root = (props) => {
-  return (
-    <Switch>
-      <Route path={'/album:ownerId(\\d+)_:albumId'} component={PhotosNext} />
-      <Route path={'/'} component={AlbumsNext} />
-    </Switch>
-  );
-};
-
-Root.propTypes = {};
-Root.defaultProps = {};
+const Root = () => (
+  <Switch>
+    <Route path={'/album:ownerId([\\d-]+)_:albumId'} component={PhotosNext} />
+    <Route path={'/'} component={AlbumsNext} />
+  </Switch>
+);
 
 export default Root;
